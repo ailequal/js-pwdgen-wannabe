7238702a718774d3b08b17f71191ee28f939746d
ailequal <37016865+ailequal@users.noreply.github.com>
2019-12-17 13:42:14 +0100

    First commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// ask the userName
+var userName = prompt("What's your name?")
+document.getElementById('user_name').innerHTML = userName;
+console.log(userName);
+// ask the userSurname
+
+// ask favorite color
+
+// print super secret password
